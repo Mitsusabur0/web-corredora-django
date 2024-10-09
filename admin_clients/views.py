650103e8c9a191d14a28f0e66c9c79bbdc358cb7
mitsusaburo <bschindlerm@gmail.com>
2024-10-09 15:11:46 -0300

    Create urls and base templates

--- admin_clients/views.py
+++ admin_clients/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def clients(request):
+    return render(request, 'admin_clients/client_list.html')
--- website_real_estate/urls.py
+++ website_real_estate/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core/urls')),
-    path('properties/', include('front_properties/urls'), namespace='properties'),
-    path('a/clients/', include('admin_clients/urls'), namespace='admin_clients'),
-    path('a/properties/', include('admin_properties/urls'), namespace='admin_properties'),
-    path('a/managements/', include('admin_managements/urls'),  namespace='admin_managements'),
+    path('', include('core.urls')),
+    # path('properties/', include('front_properties.urls')),
+    path('a/clients/', include('admin_clients.urls')),
+    # path('a/properties/', include('admin_properties.urls')),
+    # path('a/managements/', include('admin_managements.urls')),
     
 ]
